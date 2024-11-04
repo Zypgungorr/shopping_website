568489cd8edfcde60d1ae0c00ea52f8d3d22045f
simalkangalli <simal.kangalli@gmail.com>
2024-11-04 15:06:33 +0300

    committtt

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nam rem vero quam, commodi natus consequuntur pariatur. Pariatur commodi itaque soluta, culpa hic possimus, sequi quas molestias tempora quia accusantium.</p>
+      <p>simos deneme 1 23 4 5 6 7 </p>
     </div>
   );
 }
