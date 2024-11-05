38b65515281a87e760c400ff34fadbad99c74435
sura <surakaraali@gmail.com>
2024-11-05 18:51:54 +0300

    deneme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nam rem vero quam, commodi natus consequuntur pariatur. Pariatur commodi itaque soluta, culpa hic possimus, sequi quas molestias tempora quia accusantium.</p>
       <p>Şura Karaali Test</p>
       <p>DENEME</p>
       <p>header</p>
+      <p>shopping</p>
     </div>
   );
 }
